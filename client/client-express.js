13d5fbfa388ea88544e2378e60bc2484197d00f3
Suresh Balla <suresh.balla@outlook.com>
2019-07-22 07:28:03 +0530

    GRPC-sidecar implementation in k8s

---
+++ client/client-express.js
@@ -0,0 +1,30 @@
+var PROTO_PATH = __dirname + '/hello.proto';
+var grpc = require('grpc');
+var protoLoader = require('@grpc/proto-loader');
+var packageDefinition = protoLoader.loadSync(
+    PROTO_PATH,
+    {keepCase: true,
+     longs: String,
+     enums: String,
+     defaults: true,
+     oneofs: true
+    });
+var hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+const express = require('express')
+const app = express()
+const port = 3000
+function main() {
+  var client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
+  var user;
+  app.get('/:id', function (req, res) {
+    user = req.params.id;
+    client.sayHello({name: user}, function(err, response) {
+        if (err)
+            console.log('error:', err);
+      console.log('Greeting:', response.message);
+      res.send(response.message);
+    });
+  });
+  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+}
+main();
